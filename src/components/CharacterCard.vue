b56a9141666b15fd0c9f723d570592f77b60974e
leo5295 <leonardo.martellosio@gmail.com>
2023-01-17 18:08:50 +0100

    img

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -7,12 +7,15 @@ export default {
 }
 </script>
 
-<template lang="">
+<template lang="" class="mela">
   <div>
-    {{character}}
+    {{character.name}}
+  </div>
+  <div>
+    <img :src="character.card_images[0].image_url" alt="">
   </div>
 </template>
 
 <style lang="">
-  
+
 </style>
